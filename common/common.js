f0cd24fea0f7f96fcca5548604ebf899c4f83ccd
se0304 <139088294+se0304@users.noreply.github.com>
2023-08-24 16:22:27 +0900

    .

--- common/common.js
+++ common/common.js
@@ -3,10 +3,11 @@ let fadehs = document.querySelectorAll('aside a h1');
 
 fadehs.forEach(function(fadeh){
     fadeh.onmouseover = function(){
-        this.nextSibling.style.opacity = 1;
+        console.log(this.nextSibling)
+        this.nextSibling.style.opacity = 1
     }
 
     fadeh.onmouseout = function(){
-        this.nextSibling.style.opacity = 0;
+        this.nextSibling.style.opacity = 0
     }
-})
+});
