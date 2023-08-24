8c631c677354f21ae570b0b4e8ab87283017d785
se0304 <139088294+se0304@users.noreply.github.com>
2023-08-24 15:51:58 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 //main slide//
 
-$('.prev').click(function(){
+$('.prev').click(function () {
 
     $('.slide li:last').prependTo('.slide');
-    
-    $('.slide').css('margin-left','-100%');
-    
-    $('.slide').stop().animate({marginLeft:0},800);
-    
+
+    $('.slide').css('margin-left', '-100%');
+
+    $('.slide').stop().animate({ marginLeft: 0 }, 800);
+
+});
+
+
+
+$('.next').click(function () {
+
+    $('.slide').stop().animate({ marginLeft: '-100%' }, 800, function () {
+
+        $('.slide li:first').appendTo('.slide');
+
+        $('.slide').css({ marginLeft: 0 });
+
     });
-    
-    
-    
-    $('.next').click(function(){
-    
-         $('.slide').stop().animate({marginLeft:'-100%'},800, function(){
-    
-            $('.slide li:first').appendTo('.slide');
-    
-            $('.slide').css({marginLeft:0});
-    
-       });
-    
-    });
+
+});
 
 
 //slide li modal//
@@ -40,26 +40,17 @@ $(function () {
     });
 });
 
-// $(function () {
-//     $(".slide li img").click(function () {
-//         $(".main_slide img").css("opacity", "0.3");
-//     });
-
-//     $(".modal").click(function () {
-//         $(".main_slide img").css("opacity", "0.8");
-//     })
-// });
 
 //패키지 예약버튼 클릭시 팝업
 
-$(function(){
-    $(".popup").css("display", "none")
-    $(".btn").click(function(){
-        $(".popup").css("display", "block");
+$(function () {
+    $(".popup").hide();
+    $(".btn").click(function () {
+        $(".popup").show();
     });
 
-    $(".xbtn").click(function(){
-        $(".popup").css("display", "none");
+    $(".xbtn").click(function () {
+        $(".popup").hide();
     });
 });
 
@@ -67,7 +58,7 @@ $(function(){
 //팝업 셀렉트 수량 계산
 
 $(function () {
-       
+
     $(".select").change(function () {
 
         $(".txt").text($(this).val());
@@ -77,14 +68,27 @@ $(function () {
 });
 
 
-$(function(){
-    $(".pngimg").mouseover(function() {
+// 메인 슬라이드 가운데에 마우스 올리면 나타나는 설명 
+$(function () {
+    $(".pngimg").mouseover(function () {
         $(this).css("opacity", "1");
     });
 });
 
-$(function(){
-    $(".pngimg").mouseout(function(){
+$(function () {
+    $(".pngimg").mouseout(function () {
         $(this).css("opacity", "0");
+    });
+});
+
+//+ 버튼 클릭하면 백그라운드 오파시티 내려갔다가 모달 박스 이미지 클릭 하면 오파시티 원래 상태로 복귀
+
+$(function () {
+    $(".iconbox").click(function () {
+        $(".main_slide img").css("opacity", "0.5");
+    });
+
+    $(".modal").click(function () {
+        $(".main_slide img").css("opacity", "0.8");
     });
 });
